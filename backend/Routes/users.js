f42a5a92a3dc9b9e6728ceb1401558f5f79e094e
Dioris Diaz <diorid@gmail.com>
2020-04-28 19:02:35 -0400

    users added

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router()
-let User = require("../Models/user.model")
+let User = require("../Models/user.model");
+const { check, validationResult } = require("express-validator");
 
 router.route("/").get((req, res) => {
   User.find()
@@ -7,7 +8,20 @@ router.route("/").get((req, res) => {
     .catch(err => res.status(400).json("Error: " + err))
 })
 
-router.route("/add").post((req, res) => {
+router.route("/add").post(
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a vaild email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array()})
+    }
   const username = req.body.username
 
   const newUser = new User({ username })
--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 require("dotenv").config()
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5001
 
 app.use(cors())
 app.use(express.json())
@@ -22,6 +22,9 @@ const usersRouter = require("./Routes/users")
 
 app.use("/exercises", exercisesRouter)
 app.use("/users", usersRouter)
+app.use("/auth", usersRouter)
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
