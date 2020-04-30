1bb0e30f516fee199eed467322392750f23ddca2
Dioris Diaz <diorid@gmail.com>
2020-04-30 00:56:13 -0400

    form updated

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -21,7 +21,6 @@ router.route("/add").post(
     ).isLength({ min: 6 }),
   ],
   async (req, res) => {
-    console.log
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array()})
