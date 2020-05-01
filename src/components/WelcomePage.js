31a200464c75aeeec2c83340bfb3c22c92c0a048
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-05-01 17:57:26 -0400

    added pictures to the welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom"
 function WelcomePage() {
   return (
     <div>
-      <h1>Welcome to Quarantini</h1>
+      <h1>Welcome to Quarantini.</h1>
       <h5>We help you choose what to drink when you're stuck at home.</h5>
-
+      <div style={{ paddingTop: "40px" }}>
+        The outside world may be chaotic and uncertain, but in here everything
+        is simple and relaxed. You can search refreshing beverages in three
+        ways.{" "}
+      </div>
       <div style={{ paddingTop: "15px" }}>
         <Link to="/specific">Cocktail By Name</Link>. If you know the name of a
         drink but not the ingredients, this is the page you want.
@@ -18,6 +22,79 @@ function WelcomePage() {
       <div style={{ paddingTop: "10px" }}>
         <Link to="/random">Cocktail by Chance</Link>. If you're adventurous and
         up for anything, this page will give you a random cocktail recipe.
+      </div>
+      <div style={{ paddingTop: "40px", display: "flex" }}>
+        {" "}
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/bylfi21504886323.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/vqws6t1504888857.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/9uqt9p1576519019.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/wx7hsg1504370510.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/rxtqps1478251029.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/3pylqc1504370988.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
+        <img
+          src={
+            "https://www.thecocktaildb.com/images/media/drink/usuuur1439906797.jpg/preview"
+          }
+          alt="cocktail"
+          style={{
+            height: "150px",
+            width: "150px",
+            marginRight: "10px",
+          }}></img>
       </div>
     </div>
   )
