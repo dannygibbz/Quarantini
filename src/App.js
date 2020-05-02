8b2091cf3b2d53bb6a90f2c59277d610a48a08c8
Daniel Gibbons <gibbonscomedy@gmail.com>
2020-05-02 10:03:23 -0400

    messing around with footer styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
-
+import { Layout } from "antd"
 import Navbar from "./components/navbar.component"
 import DrinksList from "./components/drinks-list.component"
 import EditDrink from "./components/edit-drink.component"
--- src/components/Foot.js
+++ src/components/Foot.js
@@ -5,12 +5,19 @@ const { Footer } = Layout
 
 function Foot() {
   return (
-    <Layout style={{ marginTop: "35px" }}>
+    <Layout
+      style={{
+        marginTop: "35px",
+      }}>
       <Footer
         style={{
+          paddingTop: "5px",
           backgroundColor: "#343A40",
           color: "white",
           textAlign: "center",
+          bottom: 0,
+          width: "100%",
+          height: "40px",
         }}>
         Designed by Dioris, Donovan, and Dan. 2020 All rights reserved.
       </Footer>
